29eee7c53f5c52e72cd15c20ec22112f6ac40984
eduardo-moro <eduardomoro1127@gmail.com>
2022-02-25 12:08:59 -0300

    first layout test

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,13 +1,77 @@
 <template>
-$END$
+  <v-app-bar
+      app
+      color="#0000"
+      dark
+      elevation="0"
+      class="gradient"
+  >
+    <v-spacer/>
+
+    <div :key="link.href" v-for="link of links">
+      <v-btn
+          :href=link.href
+          :target="link.isExternal ? '_blank':''"
+          text
+      >
+        <span class="mr-2">{{ link.title }}</span>
+      </v-btn>
+    </div>
+
+    <v-spacer/>
+  </v-app-bar>
 </template>
 
 <script>
 export default {
-name: "main-header"
+  name: "main-header",
+  data() {
+    return {
+      links: [
+        {
+          href: "/",
+          title: "home",
+          isExternal: false
+        },
+        {
+          href: "/about",
+          title: "about",
+          isExternal: false
+        },
+        {
+          title: "linkedin",
+          href: "https://www.linkedin.com/in/eduardomoro/?locale=en_US",
+          isExternal: true
+        },
+        {
+          href: "https://api.whatsapp.com/send?phone=5541991855670&text=Hey%20Eduardo%2C%20I%20came%20by%20your%20github",
+          title: "whatsapp",
+          isExternal: true
+        },
+        {
+          href: "https://t.me/moroeduardo",
+          title: "telegram",
+          isExternal: true
+        },
+        {
+          href: "mailto:eduardomoro1127@gmail.com",
+          title: "gmail",
+          isExternal: true
+        },
+        {
+          href: "https://my.indeed.com/p/eduardohenriquep-hiffif5",
+          title: "indeed",
+          isExternal: true
+        },
+      ]
+    }
+  }
 }
 </script>
 
 <style scoped>
+.gradient {
+    background: linear-gradient(180deg, rgba(0,0,0,1)0%, rgba(0,0,0,0) 75%, rgba(0,0,0,0) 100%);
+}
 
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="home">
+    <v-img
+        src="../assets/view.jpeg"
+        max-height="100vh"
+    >
+      <v-container
+          fluid
+          class="white--text d-block text-md-center mt-12 font--monospace main--title"
+      >
+        <h1>Welcome, I'm Eduardo Moro</h1>
+      </v-container>
+    </v-img>
+
+    <div
+        class="py-16 black"
+    >
+
+    </div>
   </div>
 </template>
 
@@ -8,7 +25,23 @@
 
 export default {
   name: 'HomeView',
-  components: {
-  }
+  components: {}
 }
 </script>
+
+<style scoped>
+.setup-container {
+  background: black;
+  width: 100%;
+}
+
+.font--monospace {
+  font-family: monospace;
+}
+
+.main--title {
+  filter: drop-shadow(0 3px 5px black);
+
+}
+
+</style>
