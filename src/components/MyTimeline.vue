f2c751a493d29051fd8a6685cf74376dc18f09eb
eduardo-moro <eduardomoro1127@gmail.com>
2022-02-28 22:18:31 -0300

    pre layout change

---
+++ src/components/MyTimeline.vue
@@ -0,0 +1,81 @@
+<template>
+  <v-container class="">
+    <v-timeline
+        clipped
+        v-for="event in timeline"
+        :key="event.title"
+    >
+      <v-timeline-item small color="black">
+        <template v-slot:opposite>
+            <span class="headline font-weight-bold white--text">
+              {{ event.year }}
+            </span>
+        </template>
+
+        <v-card class="black">
+          <v-card-title class="text-h5 white--text">
+            {{ event.title }}
+          </v-card-title>
+
+          <v-card-text class="font-weight-bold white--text">
+            {{ event.description }}
+          </v-card-text>
+        </v-card>
+      </v-timeline-item>
+    </v-timeline>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "my-timeline",
+  data: () => {
+    return {
+      timeline: [
+        {
+          year: 2019,
+        },
+        {
+          title: "UFPR - january",
+          description: "System analysis and development"
+        },
+        {
+          title: "Revenda Mais - july",
+          description: "Backend internship"
+        },
+        {
+          year: 2020,
+        },
+        {
+          title: "Quadritech - march",
+          description: "Fullstack junior developer"
+        },
+        {
+          year: 2021
+        },
+        {
+          title: "Ecritel - june",
+          description: "Frontend mid developer"
+        },
+        {
+          title: "Mavtek - october",
+          description: "Fullstack mid developer"
+        },
+        {
+          year: 2022
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.theme--light.v-timeline::before {
+  background: white;
+  border-radius: 10px !important;
+  opacity: 35%;
+}
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,52 +3,22 @@
 
     <v-container
         fluid
-        class="white--text d-block text-md-center mt-12 font--monospace main--title"
+        class="white--text d-block text-center mt-12 font--monospace main--title"
     >
       <h1>Welcome, I'm Eduardo Moro</h1>
     </v-container>
 
-    <v-container class="mt-16">
-      <v-timeline
-          clipped
-      >
-        <v-timeline-item small color="black">
-          <template v-slot:opposite>
-            <span class="headline font-weight-bold white--text">
-              2019
-            </span>
-          </template>
-
-          <v-card class="black">
-            <v-card-title class="text-h5 white--text">
-              UFPR
-            </v-card-title>
-
-            <v-card-text class="font-weight-bold white--text">
-              Minnor degree in system analisis and development
-            </v-card-text>
-          </v-card>
-        </v-timeline-item>
-
-        <v-timeline-item small color="black">
-          <template v-slot:opposite>
-            <span class="headline font-weight-bold white--text">
-              2019
-            </span>
-          </template>
-
-          <v-card class="black ">
-            <v-card-title class="text-h5 white--text justify-end">
-              UFPR
-            </v-card-title>
-
-            <v-card-text class="font-weight-bold white--text text-end">
-              Minnor degree in system analisis and development
-            </v-card-text>
-          </v-card>
-        </v-timeline-item>
-      </v-timeline>
+    <v-container
+        fluid
+        class="white--text d-block text-center mt-12 font--monospace main--title"
+    >
+      <v-btn>
+        <p> Download CV </p>
+        <v-icon />
+      </v-btn>
     </v-container>
+
+    <my-timeline/>
 
     <div
         class="py-16 black"
@@ -61,9 +31,11 @@
 <script>
 // @ is an alias to /src
 
+import MyTimeline from "@/components/MyTimeline";
+
 export default {
   name: 'HomeView',
-  components: {}
+  components: {MyTimeline},
 }
 </script>
 
@@ -75,12 +47,6 @@ export default {
 .main--title {
   filter: drop-shadow(0 3px 5px black);
 
-}
-
-.theme--light.v-timeline::before {
-  background: white;
-  border-radius: 10px !important;
-  opacity: 35%;
 }
 
 </style>
